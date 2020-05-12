f7ad1bb4ae032df9916983a4f1e5dbd4c072e703
Bianca A. Hernandez <bhernan2@gmail.com>
2020-05-12 11:53:55 -0500

    updated

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,18 +2,49 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//console.log(tableData)
 
 //set variables
 var button = d3.select("#filter-btn");
-var inputfield1 = d3.select("#datetime");
-var inputfield2 = d3.select("#city");
-var tbody = d3.select("#body");
-var resetbtn = d3.select("#reset-btn")
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var tbody_ = d3.select("#tbody");
 var columns = ["datetime", "city", "state", "country", "sahpe", "durationMinutes", "comments"]
 
-var populate = (data) => {
-    data.ForEach(ufo_sightings => {
-        var row = tbody.append("tr");
+//input data into HTML
+var populateData = (dataInput) => {
+    dataInput.forEach(ufo_sightings => {
+        var row = tbody_.append("tr");
         columns.forEach(column => row.append("td").text(ufo_sightings[column]))
     }); 
 }
+//populate the table
+populateData(tableData);
+
+//filter by attribute
+button.on("click", () => {
+    d3.event.preventDefault();
+    var inputdate = inputFieldDate.property("value").trim();
+    var inputcity = inputFieldCity.property("value").toLowerCase().trim();
+    //filter by matching input values
+    var filterdate = data.filter(data => data.datetime === inputdate);
+    console.log(filterdate)
+    var filtercity = data.filter(data => data.city === inputcity);
+    console.log(filtercity)
+    var filterdata = data.filter(data => data.datetime === inputdate && data.city === inputcity);
+    console.log(filterdata)
+//add filtered attributes to table
+    let response = {
+        filterdata, filtercity, filterdate
+    }
+    if (response.filterdata.length !==0) {
+        populateDate(filterdata);
+    }
+    else if (response.filterdata.length === 0 && (response.filtercity.length !==0 || response.filterdate.length !== 0)){
+        populateData(filtercity) || ppopulateData(filterdate);
+    }
+    else {
+        tbody_.append("tr").append("td").text("No results found!");
+
+    }
+})
