6a26739178ae009bf5cab1a7a7b35d044889d65b
Bianca A. Hernandez <bhernan2@gmail.com>
2020-05-12 11:02:29 -0500

    set variables

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//set variables
+var button = d3.select("#filter-btn");
+var inputfield1 = d3.select("#datetime");
+var inputfield2 = d3.select("#city");
+var tbody = d3.select("#body");
+var resetbtn = d3.select("#reset-btn")
+var columns = ["datetime", "city", "state", "country", "sahpe", "durationMinutes", "comments"]
+
+var populate = (data) => {
+    data.ForEach(ufo_sightings => {
+        var row = tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufo_sightings[column]))
+    }); 
+}
